47e067693b06b6e843d91327aba8baf0d15de16a
ozakharc <ozakharc@softserveinc.com>
2018-09-27 17:45:35 +0300

    Add ViewHolder layout

--- app/src/main/java/com/example/ozakharc/redditclient/view/ListActivity.java
+++ app/src/main/java/com/example/ozakharc/redditclient/view/ListActivity.java
@@ -2,7 +2,9 @@ package com.example.ozakharc.redditclient.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.ozakharc.redditclient.ListActivityMVP;
 import com.example.ozakharc.redditclient.R;
 
 public class ListActivity extends AppCompatActivity {
